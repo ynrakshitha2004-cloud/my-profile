6c01e607fbd92d207c3aff4d690f1c3fb46a14cf
ynrakshitha2004-cloud <ynrakshitha2004@gmail.com>
2026-05-07 09:22:09 +0530

    My portfolio

--- js/data/experience.js
+++ js/data/experience.js
@@ -15,29 +15,7 @@ const experienceData = [
         duration: "2024 - 2025",
         description: "Participated in an intensive Python development workshop focused on core programming concepts",
         skills: ["Python", "OOPs Concepts", "Problem Solving", "Data Structures", "File Handling"]
-    }, {
-        type: "education",
-        title: "B.E Computer Science",
-        company: "CIT Mandya",
-        duration: "2022 - 2026",
-        description: "Focused on software engineering, DSA, and web development.",
-        skills: ["DSA", "DBMS", "OS", "Web Dev"]
-    },
-    {
-        type: "education",
-        title: "Pre-University (PCMB)",
-        company: "sadvidya PU college",
-        duration: "2021 - 2022",
-        description: "Completed pre-university studies with a focus on science and mathematics.",
-        skills: ["Mathematics", "Physics", "Chemistry", "Biology"]
-    },
-    {
-        type: "education",
-        title: "SSLC",
-        company: "Rotary High School",
-        duration: "2020 - 2021",
-        description: "Completed Secondary School Leaving Certificate with a focus on overall academic Performance",
-        skills: ["General Studies", "Languages", "Social Studies", "Science"]
-    },
+    }, 
+    
 
 ];
